8a5b3edb2aae849e401357f8b68f38a462ffe22e
gray <gray@cpan.org>
2013-06-07 22:47:45 -0700

    bump version for new release

--- lib/HTTP/Cookies/Opera.pm
+++ lib/HTTP/Cookies/Opera.pm
@@ -6,7 +6,7 @@ use warnings;
 use parent qw(HTTP::Cookies);
 use Carp qw(croak);
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 $VERSION = eval $VERSION;
 
 use constant DEBUG    => !! $ENV{HTTP_COOKIES_OPERA_DEBUG};
